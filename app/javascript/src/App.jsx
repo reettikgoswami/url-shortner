6a42bd2a924e44e8467e36594cd8e8204ecfb1c9
reettik97 <reettikgoswami1997@gmail.com>
2021-02-25 10:47:57 +0530

    Add semantic UI react

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
+import Home from "./layout/Home";
+
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => <div className="bg-yellow-700">Home page</div>}
-        />
-        <Route exact path="/about" render={() => <div>About</div>} />
+        <Route exact path="/" render={() => <Home />} />
       </Switch>
     </Router>
   );
---
+++ app/javascript/src/layout/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      this is a home component <div className="bg-yellow-700">Home page</div>
+    </div>
+  );
+};
+
+export default Home;
